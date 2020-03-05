86ef7c1d608f547eb10e065e68bc90fba9d05064
25Sucht <61848805+25Sucht@users.noreply.github.com>
2020-03-05 21:27:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.Njg1MjIwMTU2NTI3NzM4OTA2.XmFfNQ.Qonm5ZAkf4coPh_rSp1K8cBOMUY);//BOT_TOKEN is the Client Secret
+client.login(process.env.cjjLVrIuu0Hh5kM6EeHIpH5wFyycEy5T);//BOT_TOKEN is the Client Secret
